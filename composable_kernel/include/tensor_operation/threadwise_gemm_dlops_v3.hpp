245467a027c409b5dd4aba0a5fec76fe63edfc72
Jing Zhang <jizhan@amd.com>
2022-01-28 05:14:43 +0000

    added a demo of threadwise contraction

--- composable_kernel/include/tensor_operation/threadwise_gemm_dlops_v3.hpp
+++ composable_kernel/include/tensor_operation/threadwise_gemm_dlops_v3.hpp
@@ -60,6 +60,7 @@ struct ThreadwiseGemmDlops_km_kn_mn_v3
         constexpr auto I2 = Number<2>{};
         constexpr auto I3 = Number<3>{};
 
+#if 0
         constexpr auto E1 = AThreadDesc_E1_K_E2{}.GetLength(I0);
         constexpr auto K  = AThreadDesc_E1_K_E2{}.GetLength(I1);
         constexpr auto E2 = AThreadDesc_E1_K_E2{}.GetLength(I2);
@@ -107,6 +108,113 @@ struct ThreadwiseGemmDlops_km_kn_mn_v3
                 });
             });
         });
+#else
+        constexpr auto E1 = AThreadDesc_E1_K_E2{}.GetLength(I0);
+        constexpr auto E2 = AThreadDesc_E1_K_E2{}.GetLength(I2);
+
+        constexpr auto a_origin_idx = to_multi_index(AOriginIdx{});
+        constexpr auto b_origin_idx = to_multi_index(BOriginIdx{});
+        constexpr auto c_origin_idx = to_multi_index(COriginIdx{});
+
+        constexpr auto a_lengths_sub = generate_sequence_v2(
+            [&](auto i) { return AThreadDesc_E1_K_E2{}.GetLength(Number<i + 1>{}); },
+            Number<AThreadDesc_E1_K_E2{}.GetNumOfVisibleDimension() - 2>{});
+
+        constexpr auto b_lengths_sub = generate_sequence_v2(
+            [&](auto i) { return BThreadDesc_E1_N_Ho_Wo_E2{}.GetLength(Number<i + 1>{}); },
+            Number<BThreadDesc_E1_N_Ho_Wo_E2{}.GetNumOfVisibleDimension() - 2>{});
+
+        static_assert(AThreadDesc_E1_K_E2{}.GetNumOfVisibleDimension() == 3, "");
+        static_assert(BThreadDesc_E1_N_Ho_Wo_E2{}.GetNumOfVisibleDimension() == 5, "");
+
+        static_assert(a_lengths_sub.Size() == 1, "");
+        static_assert(b_lengths_sub.Size() == 3, "");
+
+        static_for<0, E1, 1>{}([&](auto e1) {
+            static_ford<decltype(a_lengths_sub)>{}([&](auto a_idx_sub) {
+                static_ford<decltype(b_lengths_sub)>{}([&](auto b_idx_sub) {
+                    vector_type<FloatA, E2> a_vec;
+                    vector_type<FloatB, E2> b_vec;
+
+                    static_for<0, E2, 1>{}([&](auto e2) {
+                        constexpr auto a_idx = generate_tuple(
+                            [&](auto i) {
+                                if constexpr(i == 0)
+                                {
+                                    return Number<e1>{};
+                                }
+                                else if constexpr(i ==
+                                                  AThreadDesc_E1_K_E2{}.GetNumOfVisibleDimension() -
+                                                      1)
+                                {
+                                    return Number<e2>{};
+                                }
+                                else
+                                {
+                                    return a_idx_sub[i - 1];
+                                }
+                            },
+                            Number<AThreadDesc_E1_K_E2{}.GetNumOfVisibleDimension()>{});
+
+                        constexpr index_t a_offset =
+                            AThreadDesc_E1_K_E2{}.CalculateOffset(a_origin_idx + a_idx);
+
+                        a_vec.template AsType<FloatA>()(Number<e2>{}) = a_buf[Number<a_offset>{}];
+
+                        constexpr auto b_idx = generate_tuple(
+                            [&](auto i) {
+                                if constexpr(i == 0)
+                                {
+                                    return Number<e1>{};
+                                }
+                                else if constexpr(i == BThreadDesc_E1_N_Ho_Wo_E2{}
+                                                               .GetNumOfVisibleDimension() -
+                                                           1)
+                                {
+                                    return Number<e2>{};
+                                }
+                                else
+                                {
+                                    return b_idx_sub[i - 1];
+                                }
+                            },
+                            Number<BThreadDesc_E1_N_Ho_Wo_E2{}.GetNumOfVisibleDimension()>{});
+
+                        constexpr index_t b_offset =
+                            BThreadDesc_E1_N_Ho_Wo_E2{}.CalculateOffset(b_origin_idx + b_idx);
+
+                        b_vec.template AsType<FloatB>()(Number<e2>{}) = b_buf[Number<b_offset>{}];
+                    });
+
+                    // create c_idx = {a_idx_sub, b_idx_sub}
+                    constexpr auto c_idx = generate_tuple(
+                        [&](auto i) {
+                            if constexpr(i < a_idx_sub.Size())
+                            {
+                                return a_idx_sub[i];
+                            }
+                            else
+                            {
+                                return b_idx_sub[Number<i - a_idx_sub.Size()>{}];
+                            }
+                        },
+                        Number<CThreadDesc_K_N_Ho_Wo{}.GetNumOfVisibleDimension()>{});
+
+                    constexpr index_t c_offset =
+                        CThreadDesc_K_N_Ho_Wo{}.CalculateOffset(c_origin_idx + c_idx);
+
+                    using a_vec_t = typename vector_type<FloatA, E2>::type;
+                    using b_vec_t = typename vector_type<FloatB, E2>::type;
+
+                    inner_product<a_vec_t, b_vec_t, FloatC>(
+                        a_vec.template AsType<a_vec_t>()[Number<0>{}],
+                        b_vec.template AsType<b_vec_t>()[Number<0>{}],
+                        c_buf(Number<c_offset>{}));
+                });
+            });
+        });
+
+#endif
     }
 };
 
