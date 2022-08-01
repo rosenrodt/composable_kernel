a83df130281c50c1fa744f58e3319d17bab56d73
Anthony Chang <ac.chang@outlook.com>
2022-08-01 18:09:35 +0800

    fused attention: bench softmax

--- bench_gemm_gemm.sh
+++ bench_gemm_gemm.sh
@@ -8,6 +8,8 @@ if [ $3 ]; then B=$3; else B=16; fi
 # Gemm0: TN
 $PROFILER batched_gemm 1 1 0 2 0 1 $N $N $D $D $D $N -1 -1 -1 $B
 
+# Softmax standalone
+$PROFILER softmax 1 0 1 0 1 --length $B $N $N --reduce 2
 
 # Gemm1: TT
 $PROFILER batched_gemm 1 0 0 2 0 1 $N $D $N $N $D $D -1 -1 -1 $B
