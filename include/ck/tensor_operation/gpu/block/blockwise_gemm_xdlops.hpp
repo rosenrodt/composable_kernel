357df51e662dfed2ffc8de64e314503e75d4e470
Anthony Chang <ac.chang@outlook.com>
2022-08-01 17:05:13 +0800

    transpose both gemm0 and gemm1 xdl output so as to avoid broadcasting softmax max/sum

--- include/ck/tensor_operation/gpu/block/blockwise_gemm_xdlops.hpp
+++ include/ck/tensor_operation/gpu/block/blockwise_gemm_xdlops.hpp
@@ -131,7 +131,9 @@ struct BlockwiseGemmXdlops_k0mk1_k0nk1_m0n0m1n1m2m3m4n2_v1
         const auto waveId_m = wave_idx[I0];
         const auto waveId_n = wave_idx[I1];
 
-        const auto blk_idx = xdlops_gemm.GetBeginOfThreadBlk(xdlops_i, blk_i);
+        const auto tmp = xdlops_gemm.GetBeginOfThreadBlk(xdlops_i, blk_i);
+        const auto blk_idx =
+            TransposeC ? make_multi_index(tmp[I1], tmp[I0]) : make_multi_index(tmp[I0], tmp[I1]);
 
         constexpr auto mrepeat_mwave_mperxdl_to_m_adaptor = make_single_stage_tensor_adaptor(
             make_tuple(make_unmerge_transform(make_tuple(MRepeat, MWaves, MPerXDL))),
