145b4923c1cda8e5a19e3ab8797e5e1074b0a0f4
Anthony Chang <ac.chang@outlook.com>
2022-08-03 12:47:59 +0800

    clean up

--- library/include/ck/library/utility/check_err.hpp
+++ library/include/ck/library/utility/check_err.hpp
@@ -137,7 +137,7 @@ check_err(const std::vector<T>& out,
         {
             max_err = err > max_err ? err : max_err;
             err_count++;
-            if(err_count < 128)
+            if(err_count < 5)
             {
                 std::cout << std::setw(12) << std::setprecision(7) << "out[" << i << "] != ref["
                           << i << "]: " << o << " != " << r << std::endl
