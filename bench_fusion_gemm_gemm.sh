86d59cdd49567ba8d94d1cf8b8cf5550654cbf38
Anthony Chang <ac.chang@outlook.com>
2022-08-04 16:52:03 +0800

    remove bench script

--- bench_fusion_gemm_gemm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PROFILER=build/bin/example_gemm_gemm_xdl_fp16
-
-if [ $1 ]; then N=$1; else N=4096; fi
-if [ $2 ]; then D=$2; else D=64; fi
-if [ $3 ]; then B=$3; else B=16; fi
-
-$PROFILER 0 1 1 $N $N $D $D $D $D $D $D $B
