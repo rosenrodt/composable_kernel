6b90f8dddd1650a6c5a0e01146e9cf21c284d9c9
Anthony Chang <ac.chang@outlook.com>
2022-08-09 14:50:13 +0800

    add gemm_softmax_gemm instances and tests

--- include/ck/tensor_operation/gpu/device/device_batched_gemm_softmax_gemm.hpp
+++ include/ck/tensor_operation/gpu/device/device_batched_gemm_softmax_gemm.hpp
@@ -24,7 +24,7 @@ template <typename ALayout,
           typename B0ElementwiseOperation,
           typename B1ElementwiseOperation,
           typename CElementwiseOperation>
-struct DeviceBatchedGemmGemm : public BaseOperator
+struct DeviceBatchedGemmSoftmaxGemm : public BaseOperator
 {
     virtual std::unique_ptr<BaseArgument>
     MakeArgumentPointer(const void* p_a,
@@ -64,18 +64,19 @@ template <typename ALayout,
           typename B0ElementwiseOperation,
           typename B1ElementwiseOperation,
           typename CElementwiseOperation>
-using DeviceBatchedGemmGemmPtr = std::unique_ptr<DeviceBatchedGemmGemm<ALayout,
-                                                                       B0Layout,
-                                                                       B1Layout,
-                                                                       CLayout,
-                                                                       ADataType,
-                                                                       B0DataType,
-                                                                       B1DataType,
-                                                                       CDataType,
-                                                                       AElementwiseOperation,
-                                                                       B0ElementwiseOperation,
-                                                                       B1ElementwiseOperation,
-                                                                       CElementwiseOperation>>;
+using DeviceBatchedGemmSoftmaxGemmPtr =
+    std::unique_ptr<DeviceBatchedGemmSoftmaxGemm<ALayout,
+                                                 B0Layout,
+                                                 B1Layout,
+                                                 CLayout,
+                                                 ADataType,
+                                                 B0DataType,
+                                                 B1DataType,
+                                                 CDataType,
+                                                 AElementwiseOperation,
+                                                 B0ElementwiseOperation,
+                                                 B1ElementwiseOperation,
+                                                 CElementwiseOperation>>;
 
 } // namespace device
 } // namespace tensor_operation
