3bea04d80294b88da40a1ca88dd41ffd99171468
Anthony Chang <ac.chang@outlook.com>
2022-08-18 18:21:11 +0800

    refactor matrix padder

--- include/ck/tensor_operation/gpu/device/matrix_padder.hpp
+++ include/ck/tensor_operation/gpu/device/matrix_padder.hpp
@@ -12,6 +12,36 @@ namespace ck {
 namespace tensor_operation {
 namespace device {
 
+template <bool PadM,
+          bool PadN,
+          typename TensorDesc_MRaw_NRaw,
+          typename MPerBlockType,
+          typename NPerBlockType>
+__host__ __device__ constexpr auto
+PadTensorDescriptor_M_N(const TensorDesc_MRaw_NRaw& tensor_desc_mraw_nraw,
+                        MPerBlockType MPerBlock,
+                        NPerBlockType NPerBlock)
+{
+    const auto MRaw = tensor_desc_mraw_nraw.GetLength(Number<0>{});
+    const auto NRaw = tensor_desc_mraw_nraw.GetLength(Number<1>{});
+
+    const auto M = math::integer_divide_ceil(MRaw, MPerBlock) * MPerBlock;
+    const auto N = math::integer_divide_ceil(NRaw, NPerBlock) * NPerBlock;
+
+    const auto MPad = M - MRaw;
+    const auto NPad = N - NRaw;
+
+    const auto MTransform = conditional_expr<PadM>(make_right_pad_transform(MRaw, MPad),
+                                                   make_pass_through_transform(MRaw));
+    const auto NTransform = conditional_expr<PadN>(make_right_pad_transform(NRaw, NPad),
+                                                   make_pass_through_transform(NRaw));
+
+    return transform_tensor_descriptor(tensor_desc_mraw_nraw,
+                                       make_tuple(MTransform, NTransform),
+                                       make_tuple(Sequence<0>{}, Sequence<1>{}),
+                                       make_tuple(Sequence<0>{}, Sequence<1>{}));
+}
+
 // M/N/KPerTileType could be index_t or Number<>
 template <GemmSpecialization GemmSpec,
           typename MPerTileType,
@@ -24,154 +54,35 @@ struct MatrixPadder
     static constexpr auto I2 = Number<2>{};
     static constexpr auto I3 = Number<3>{};
 
+    static constexpr bool PadM =
+        (GemmSpec == GemmSpecialization::MPadding || GemmSpec == GemmSpecialization::MNPadding ||
+         GemmSpec == GemmSpecialization::MKPadding || GemmSpec == GemmSpecialization::MNKPadding);
+    static constexpr bool PadN =
+        (GemmSpec == GemmSpecialization::NPadding || GemmSpec == GemmSpecialization::MNPadding ||
+         GemmSpec == GemmSpecialization::NKPadding || GemmSpec == GemmSpecialization::MNKPadding);
+    static constexpr bool PadK =
+        (GemmSpec == GemmSpecialization::KPadding || GemmSpec == GemmSpecialization::MKPadding ||
+         GemmSpec == GemmSpecialization::NKPadding || GemmSpec == GemmSpecialization::MNKPadding);
+
     template <typename ADesc_MRaw_KRaw>
     __host__ __device__ constexpr auto
     PadADescriptor_M_K(const ADesc_MRaw_KRaw& a_desc_mraw_kraw) const
     {
-        const auto MRaw = a_desc_mraw_kraw.GetLength(I0);
-        const auto KRaw = a_desc_mraw_kraw.GetLength(I1);
-
-        const auto M = math::integer_divide_ceil(MRaw, MPerTile_) * MPerTile_;
-        const auto K = math::integer_divide_ceil(KRaw, KPerTile_) * KPerTile_;
-
-        const auto MPad = M - MRaw;
-        const auto KPad = K - KRaw;
-
-        if constexpr(GemmSpec == GemmSpecialization::MKPadding ||
-                     GemmSpec == GemmSpecialization::MNKPadding)
-        {
-            // pad both M and K
-            return transform_tensor_descriptor(a_desc_mraw_kraw,
-                                               make_tuple(make_right_pad_transform(MRaw, MPad),
-                                                          make_right_pad_transform(KRaw, KPad)),
-                                               make_tuple(Sequence<0>{}, Sequence<1>{}),
-                                               make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else if constexpr(GemmSpec == GemmSpecialization::MPadding ||
-                          GemmSpec == GemmSpecialization::MNPadding)
-        {
-            // pad M, but not K
-            return transform_tensor_descriptor(
-                a_desc_mraw_kraw,
-                make_tuple(make_right_pad_transform(MRaw, MPad), make_pass_through_transform(KRaw)),
-                make_tuple(Sequence<0>{}, Sequence<1>{}),
-                make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else if constexpr(GemmSpec == GemmSpecialization::KPadding ||
-                          GemmSpec == GemmSpecialization::NKPadding)
-        {
-            // pad K, but not M
-            return transform_tensor_descriptor(
-                a_desc_mraw_kraw,
-                make_tuple(make_pass_through_transform(MRaw), make_right_pad_transform(KRaw, KPad)),
-                make_tuple(Sequence<0>{}, Sequence<1>{}),
-                make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else
-        {
-            // not pad M or K
-            return a_desc_mraw_kraw;
-        }
+        return PadTensorDescriptor_M_N<PadM, PadK>(a_desc_mraw_kraw, MPerTile_, KPerTile_);
     }
 
     template <typename BDesc_NRaw_KRaw>
     __host__ __device__ constexpr auto
     PadBDescriptor_N_K(const BDesc_NRaw_KRaw& b_desc_nraw_kraw) const
     {
-        const auto NRaw = b_desc_nraw_kraw.GetLength(I0);
-        const auto KRaw = b_desc_nraw_kraw.GetLength(I1);
-
-        const auto N = math::integer_divide_ceil(NRaw, NPerTile_) * NPerTile_;
-        const auto K = math::integer_divide_ceil(KRaw, KPerTile_) * KPerTile_;
-
-        const auto NPad = N - NRaw;
-        const auto KPad = K - KRaw;
-
-        if constexpr(GemmSpec == GemmSpecialization::NKPadding ||
-                     GemmSpec == GemmSpecialization::MNKPadding)
-        {
-            // pad both N and K
-            return transform_tensor_descriptor(b_desc_nraw_kraw,
-                                               make_tuple(make_right_pad_transform(NRaw, NPad),
-                                                          make_right_pad_transform(KRaw, KPad)),
-                                               make_tuple(Sequence<0>{}, Sequence<1>{}),
-                                               make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else if constexpr(GemmSpec == GemmSpecialization::NPadding ||
-                          GemmSpec == GemmSpecialization::MNPadding)
-        {
-            // pad N, but not K
-            return transform_tensor_descriptor(
-                b_desc_nraw_kraw,
-                make_tuple(make_right_pad_transform(NRaw, NPad), make_pass_through_transform(KRaw)),
-                make_tuple(Sequence<0>{}, Sequence<1>{}),
-                make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else if constexpr(GemmSpec == GemmSpecialization::KPadding ||
-                          GemmSpec == GemmSpecialization::MKPadding)
-        {
-            // pad K, but not N
-            return transform_tensor_descriptor(
-                b_desc_nraw_kraw,
-                make_tuple(make_pass_through_transform(NRaw), make_right_pad_transform(KRaw, KPad)),
-                make_tuple(Sequence<0>{}, Sequence<1>{}),
-                make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else
-        {
-            // not pad N or K
-            return b_desc_nraw_kraw;
-        }
+        return PadTensorDescriptor_M_N<PadN, PadK>(b_desc_nraw_kraw, NPerTile_, KPerTile_);
     }
 
     template <typename CDesc_MRaw_NRaw>
     __host__ __device__ constexpr auto
     PadCDescriptor_M_N(const CDesc_MRaw_NRaw& c_desc_mraw_nraw) const
     {
-        const auto MRaw = c_desc_mraw_nraw.GetLength(I0);
-        const auto NRaw = c_desc_mraw_nraw.GetLength(I1);
-
-        const auto M = math::integer_divide_ceil(MRaw, MPerTile_) * MPerTile_;
-        const auto N = math::integer_divide_ceil(NRaw, NPerTile_) * NPerTile_;
-
-        const auto MPad = M - MRaw;
-        const auto NPad = N - NRaw;
-
-        if constexpr(GemmSpec == GemmSpecialization::MNPadding ||
-                     GemmSpec == GemmSpecialization::MNKPadding)
-        {
-            // pad M and N
-            return transform_tensor_descriptor(c_desc_mraw_nraw,
-                                               make_tuple(make_right_pad_transform(MRaw, MPad),
-                                                          make_right_pad_transform(NRaw, NPad)),
-                                               make_tuple(Sequence<0>{}, Sequence<1>{}),
-                                               make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else if constexpr(GemmSpec == GemmSpecialization::MPadding ||
-                          GemmSpec == GemmSpecialization::MKPadding)
-        {
-            // pad M, but not N
-            return transform_tensor_descriptor(
-                c_desc_mraw_nraw,
-                make_tuple(make_right_pad_transform(MRaw, MPad), make_pass_through_transform(NRaw)),
-                make_tuple(Sequence<0>{}, Sequence<1>{}),
-                make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else if constexpr(GemmSpec == GemmSpecialization::NPadding ||
-                          GemmSpec == GemmSpecialization::NKPadding)
-        {
-            // pad N, but not M
-            return transform_tensor_descriptor(
-                c_desc_mraw_nraw,
-                make_tuple(make_pass_through_transform(MRaw), make_right_pad_transform(NRaw, NPad)),
-                make_tuple(Sequence<0>{}, Sequence<1>{}),
-                make_tuple(Sequence<0>{}, Sequence<1>{}));
-        }
-        else
-        {
-            // not pad M or N
-            return c_desc_mraw_nraw;
-        }
+        return PadTensorDescriptor_M_N<PadM, PadN>(c_desc_mraw_nraw, MPerTile_, NPerTile_);
     }
 
     MPerTileType MPerTile_;
